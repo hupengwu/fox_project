8d0d7a467066617112fd7d2d06c6728e5e419212
hupengwu <5389408@qq.com>
2020-10-30 17:22:07 +0800

    SerialPortService

---
+++ SerialPortService/CUDPItemClientSocket.cpp
@@ -0,0 +1,101 @@
+#include "CUDPItemClientSocket.h"
+
+/////////////////////////////////////////////////////////////////////////////
+// CUDPBuffClientSocket
+
+BYTE							CUDPItemClientSocket::m_RxBuff[4096];
+FoxPThreadMutex					CUDPItemClientSocket::m_Lock;
+DWORDLONG						CUDPItemClientSocket::m_dlRxCount = 0;
+SOCKADDR						CUDPItemClientSocket::m_SockAddr;
+
+CUDPItemClientSocket::CUDPItemClientSocket()
+{
+}
+
+CUDPItemClientSocket::~CUDPItemClientSocket()
+{
+}
+
+
+void CUDPItemClientSocket::OnReceive(int nErrorCode)
+{
+	FoxPThreadAutoLock AutoLock(m_Lock);
+
+	FoxString		strIP;
+	UINT		nPort;
+	FoxString		strHead;
+	char		chrHead[12];
+
+	// 接收数据
+	int			iSockAddrLen = sizeof(m_SockAddr);
+	int nCount = FoxUdpSocket::ReceiveFrom(m_RxBuff, 4096, &m_SockAddr, &iSockAddrLen);
+	if (nCount == SOCKET_ERROR) {
+		DWORD dwError = FoxUdpSocket::GetLastError();
+		FoxUdpSocket::OnReceive(nErrorCode);
+
+		/*
+		if (CBassVIParam::m_bDebugMessage)
+		{
+			FoxString strError;
+			strError.Format("接收错误:ERROR=%d",dwError);
+		}
+		*/
+
+		return;
+	}
+	strIP.Format("%d.%d.%d.%d",
+		(BYTE)(m_SockAddr.sa_data[2]),
+		(BYTE)(m_SockAddr.sa_data[3]),
+		(BYTE)(m_SockAddr.sa_data[4]),
+		(BYTE)(m_SockAddr.sa_data[5]));
+	nPort = (BYTE)(m_SockAddr.sa_data[0]) * 0x100 + (BYTE)(m_SockAddr.sa_data[1]);
+
+	// 合法性检查:数据长度
+	if (nCount < 8)
+	{
+		FoxUdpSocket::OnReceive(nErrorCode);
+		return;
+	}
+
+	// 获取头部:数据类型	
+
+	memcpy(chrHead, m_RxBuff, 8);
+	chrHead[8] = 0;
+
+	strHead = chrHead;
+	if (strHead == "[SERVER]")
+	{
+		// 添加到队列
+		// 临时取消
+	//	CQueueUDPClient::RecvBuff(strIP, nPort, m_RxBuff, nCount);
+	}
+
+	m_dlRxCount++;
+	FoxUdpSocket::OnReceive(nErrorCode);
+}
+
+DWORD CUDPItemClientSocket::GetRxCount()
+{
+	return m_dlRxCount;
+}
+
+// 函数说明:重载SendToHelper,以避免执行父类CSocket的同名函数
+int CUDPItemClientSocket::SendToHelper(const void* lpBuf, int nBufLen, const SOCKADDR* lpSockAddr, int nSockAddrLen, int nFlags)
+{
+	/*if (m_pbBlocking != NULL)
+	{
+		WSASetLastError(WSAEINPROGRESS);
+		return SOCKET_ERROR;
+	}
+*/
+	int nResult;
+	while ((nResult = FoxUdpSocket::SendToHelper(lpBuf, nBufLen, lpSockAddr, nSockAddrLen, nFlags)) == SOCKET_ERROR)
+	{
+		FoxUdpSocket::GetLastError();
+		return SOCKET_ERROR;
+	}
+
+	return nResult;
+}
+
+
---
+++ SerialPortService/CUDPItemClientSocket.h
@@ -0,0 +1,42 @@
+#pragma once
+
+
+#include <FoxUdpSocket.h>
+#include <FoxPThreadAutoLock.h>
+
+class CUDPItemClientSocket : public FoxUdpSocket
+{
+	// Attributes
+public:
+
+public:
+	//	int SendTo(const void* lpBuf, int nBufLen, UINT nHostPort, LPCTSTR lpszHostAddress, int nFlags);
+
+	//	int SendTo(const void* lpBuf, int nBufLen, const SOCKADDR* lpSockAddr, int nSockAddrLen, int nFlags);
+
+	// Operations
+public:
+	CUDPItemClientSocket();
+	virtual ~CUDPItemClientSocket();
+
+	// Overrides
+public:
+	static DWORD GetRxCount();
+
+public:
+	virtual void OnReceive(int nErrorCode);
+
+protected:
+	// 重载SendToHelper,以避免执行父类CSocket的同名函数
+	virtual int SendToHelper(const void* lpBuf, int nBufLen, const SOCKADDR* lpSockAddr, int nSockAddrLen, int nFlags);
+
+	// Implementation
+protected:
+	static	BYTE								m_RxBuff[4096];
+	static	FoxPThreadMutex						m_Lock;
+	static	SOCKADDR							m_SockAddr;
+
+	static DWORDLONG							m_dlRxCount;
+};
+
+
